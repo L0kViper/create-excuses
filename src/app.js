aa0ebad61a4aa95df80f8faa5db7a5c4a2827102
Cesar Contreras <cesar.alberto2003@outlook.com>
2024-07-17 01:05:31 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,32 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  function randomly(list) {
+    let numAleatorio = Math.floor(Math.random() * list.length);
+    return list[numAleatorio] + " ";
+  }
+
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let excusa =
+    randomly(who) + randomly(action) + randomly(what) + randomly(when);
+  let Elementexcuse = document.querySelector("#excuse");
+
+  Elementexcuse.innerHTML = excusa;
+
+  let ButtonExcuse = document.querySelector("#GenerateExcuses");
+
+  ButtonExcuse.addEventListener("click", () => {
+    let NewExcuse =
+      randomly(who) + randomly(action) + randomly(what) + randomly(when);
+    Elementexcuse.innerHTML = NewExcuse;
+  });
 };
